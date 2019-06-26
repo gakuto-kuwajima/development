7ee61e8a8f9e7e967a5c3c6a6e99c15d9198e2d8
gakuto <mydance365@gmail.com>
2019-06-27 08:20:41 +0900

    20190626 mac 3

--- nshwebsite/resources/views/community/show.blade.php
+++ nshwebsite/resources/views/community/show.blade.php
@@ -3,33 +3,33 @@
 @section('content')
 
 <div class="container">
-    <div class="eyecatch">
-        <div class="row">
+    <div class="form-group row">
+        <div class="eyecatch">
             <div class="col-md-12 mx-auto">
-              @if (!is_null($page->eyecatch_path))
+                @if (!is_null($page->eyecatch_path))
                 <img src="{{ asset('storage/image/' . $page->eyecatch_path) }}" id='eyecatch' class="img-fluid">
-              @endif
+                @endif
             </div>
         </div>
     </div>
-    <div class="community_name">
-        <div class="row">
+
+        <div class="community_name">
             <div class="col-md-12">
                 <h1　class="text-center">{{ $page->name }}</h1>
             </div>
         </div>
-    </div>
-    <div class="information">
-        <h2 class="pagemidashi2">活動情報</h2>
-        <div class="row">
+
+    <div class="form-group row">
+        <div class="information">
+            <h2 class="pagemidashi2">活動情報</h2>
             <div class="text col-md-12">
                 <p>{{ $page->information }}</p>
             </div>
         </div>
     </div>
-    <div class="message">
-        <h2 class="pagemidashi2">メッセージ</h2>
-        <div class="row">
+    <div class="form-group row">
+        <div class="message">
+            <h2 class="pagemidashi2">メッセージ</h2>
             <div class="balloon">
                 <div class="col-md-12">
                 @if (!is_null($page->message_image_path))
@@ -46,22 +46,26 @@
             </div>
         </div>
     </div>
-    <div class="contact">
-        <h2 class="pagemidashi2">お問い合わせ</h2>
-        <div class="row">
+    <div class="form-group row">
+        <div class="contact">
+            <h2 class="pagemidashi2">お問い合わせ</h2>
             <div class="text col-md-12">
                 <p>{{ $page->contact }}</p>
             </div>
         </div>
     </div>
-    <div class="facebook_link">
-        <a href="{{ $page->facebook_link }}" class="btn-square">Facebookページへ</a>
-    </div>
-    <div class="video_link">
-      <div class="col-md-6 mx-auto">
-        <div class="embed-responsive embed-responsive-16by9">
-            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/{{ $page->video_link }}" allowfullscreen></iframe>
+    <div class="form-group row">
+        <div class="facebook_link">
+            <a href="{{ $page->facebook_link }}" class="btn-square">Facebookページへ</a>
         </div>
-      </div>
     </div>
+
+        <div class="video_link">
+            <div class="col-md-6 mx-auto">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/{{ $page->video_link }}" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+
 @endsection
