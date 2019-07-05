8b25e3ab07601b818772096415815ff7ab8d2f17
gakuto <mydance365@gmail.com>
2019-07-05 17:33:43 +0900

    20190705 windows

--- nshwebsite/resources/views/community/index.blade.php
+++ nshwebsite/resources/views/community/index.blade.php
@@ -6,9 +6,23 @@
         <div class="row">
             <h2>作成コミュニティ一覧</h2>
         </div>
-        <div class="row py-4">
+        <div class="row">
             <div class="col-md-4">
                 <a href="{{ action('Admin\CommunityController@add') }}" role="button" class="btn btn-primary">新規作成</a>
+            </div>
+            <div class="col-md-8">
+                <form action="{{ action('Admin\CommunityController@index')}}" method="get">
+                    <div class="form-group row">
+                        <label class="col-md-2">タイトル</label>
+                        <div class="col-md-8">
+                            <input type="text" class="form-control" name="cond_title" value={{ $cond_title }}>
+                        </div>
+                        <div class="col-md-2">
+                            {{ csrf_field()}}
+                            <input type="submit" class="btn btn-primary" value="検索">
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
         <div class="row">
